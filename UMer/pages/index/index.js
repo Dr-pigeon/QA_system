e9f98ad924dc2bcbdd6f48f87b5c94c9362d8cc1
MagicianChen <44462240+MagicianChen@users.noreply.github.com>
2019-11-06 13:22:39 +0800

    Add files via upload

---
+++ UMer/pages/index/index.js
@@ -0,0 +1,282 @@
+var util = require('../../utils/util')
+const app = getApp()
+var emojis = app.globalData.emojis
+Page({
+  data: {
+    messages: [],
+    isSpeech: false,
+    scrollHeight: 0,
+    toView: '',
+    windowHeight: 0,
+    windowWidth: 0,
+    pxToRpx: 2,
+    msg: '',
+    emotionBox: false,
+    emotions: [],
+    speechText: '按住 说话',
+    changeImageUrl: '/images/voice.png',
+    speechIcon: '/images/speech0.png',
+    defaultSpeechIcon: '/images/speech0.png',
+    emotionIcon: '/images/emotion.png',
+    playingSpeech: ''
+  },
+  chooseEmotion(e) {
+    this.setData({
+      msg: this.data.msg + '[' + e.target.dataset.name + ']',
+    })
+  },
+  sendMessage(e) {
+    this.setData({
+      msg: e.detail.value,
+    })
+  },
+  onLoad() {
+    var that = this
+    let emotions = []
+    for (let i = 0; i < emojis.length; i++) {
+      emotions.push({
+        src: '/emoji/' + util.getEmojiEn(emojis[i]) + '.png',
+        id: i,
+        name: emojis[i]
+      })
+    }
+    this.setData({
+      emotions: emotions
+    })
+    wx.getSystemInfo({
+      success: (res) => {
+        this.setData({
+          windowHeight: res.windowHeight,
+          pxToRpx: 750 / res.screenWidth,
+          scrollHeight: (res.windowHeight - 50) * 750 / res.screenWidth
+        })
+      }
+    })
+  },
+  onShareAppMessage: function () {
+    return {
+      title: 'UMor',
+      path: '/pages/index/index'
+    }
+  },
+  emotionBtn() {
+    if (this.data.emotionBox) {
+      this.setData({
+        emotionBox: false,
+        scrollHeight: (this.data.windowHeight - 50) * this.data.pxToRpx
+      })
+    } else {
+      this.setData({
+        emotionBox: true,
+        scrollHeight: (this.data.windowHeight - 285) * this.data.pxToRpx
+      })
+      if (this.data.isSpeech) {
+        this.setData({
+          isSpeech: false,
+          changeImageUrl: '/images/voice.png'
+        });
+      }
+    }
+  }, changeType: function () {
+    if (this.data.isSpeech) {
+      this.setData({
+        isSpeech: false,
+        changeImageUrl: '/images/voice.png'
+      });
+    } else {
+      this.setData({
+        isSpeech: true,
+        changeImageUrl: '/images/keyinput.png',
+        emotionBox: false,
+        scrollHeight: (this.data.windowHeight - 50) * this.data.pxToRpx
+      });
+    }
+  },
+  send: function () {
+    var that = this;
+    let msg = this.data.msg
+    let contents = util.getContents(msg)
+    let id = 'id_' + Date.parse(new Date()) / 1000;
+    let data = { id: id, contents: contents, me: true, avatar: wx.getStorageSync('userInfo').avatarUrl, speech: false }
+    let messages = this.data.messages
+    messages.push(data)
+    this.setData({
+      messages: messages,
+      msg: ''
+    })
+    this.setData({
+      toView: id
+    })
+    wx.request({
+      url: 'https://www.kenchan.net.cn:13011/applet',
+      method: 'POST',
+      data: { "text": data.contents[0]['text'], 'action':'text' },
+      header:{
+      'Content-Type':'application/json'
+      },
+      success: function (res) {
+        if (res.statusCode == 200) {
+          let answer = res.data.text;
+          let contents = util.getContents(answer)
+          let id = 'id_' + Date.parse(new Date()) / 1000;
+          let data = { id: id, contents: contents, me: false, avatar: '/images/robot.jpg', speech: false }
+          let messages = that.data.messages
+          messages.push(data)
+          console.log(messages)
+          that.setData({
+            messages: messages
+          })
+          that.setData({
+            toView: id
+          })
+        }
+      },
+      fail: function (res) {
+        console.log(res)
+      }
+    })
+  },
+  startRecord: function () {
+    var that = this;
+    this.setData({
+      speechText: '松开 发送'
+    })
+    var seconds = 0;
+    var interval = setInterval(function () {
+      seconds++
+    }, 1000);
+    wx.startRecord({
+      success: function (res) {
+        clearInterval(interval);
+        var tempFilePath = res.tempFilePath
+        seconds = seconds == 0 ? 1 : seconds;
+        let id = 'id_' + Date.parse(new Date()) / 1000;
+        let data = { id: id, me: true, avatar: wx.getStorageSync('userInfo').avatarUrl, speech: true, seconds: seconds, filePath: tempFilePath }
+        let messages = that.data.messages
+        messages.push(data)
+        that.setData({
+          messages: messages
+        });
+        that.setData({
+          toView: id
+        })
+        let nickName = wx.getStorageSync('userInfo').nickName;
+        if (!nickName) nickName = 'null';
+        wx.uploadFile({
+          url:'http://www.kenchan.net.cn:13011/applet',
+          filePath: tempFilePath,
+          name: 'voice',
+          formData: {
+            'action': 'voice',
+            'userid': wx.getStorageSync('openid'),
+            'username': wx.getStorageSync('userInfo').nickName
+          },
+          success: function (res) {
+            let resData = JSON.parse(res.data);
+            if (resData.code == 102) {
+              let answer = resData.text;
+              let contents = util.getContents(answer)
+              let id = 'id_' + Date.parse(new Date()) / 1000;
+              let data = { id: id, contents: contents, me: false, avatar: '/images/robot.jpg', speech: false }
+              let messages = that.data.messages
+              messages.push(data)
+              that.setData({
+                messages: messages
+              })
+              that.setData({
+                toView: id
+              })
+            } else if (resData.code == 101) {
+              var isFirst = true;
+              wx.playBackgroundAudio({
+                dataUrl: host + '/static/' + resData.text
+              });
+              wx.onBackgroundAudioPlay(function () {
+                wx.getBackgroundAudioPlayerState({
+                  success: function (res) {
+                    if (!isFirst) {
+                      return;
+                    }
+                    isFirst = false;
+                    let duration = res.duration;
+                    wx.stopBackgroundAudio();
+                    let id = 'id_' + Date.parse(new Date()) / 1000;
+                    let data = { id: id, me: false, avatar: '/images/robot.jpg', speech: true, seconds: duration == 0 ? 1 : duration, filePath: host + '/static/' + resData.text }
+                    let messages = that.data.messages
+                    messages.push(data)
+                    that.setData({
+                      messages: messages
+                    });
+                    that.setData({
+                      toView: id
+                    })
+                  }
+                })
+              });
+            }
+          },
+          fail: function (err) {
+            console.log(err)
+          }
+        })
+      },
+      fail: function (err) {
+        console.log(err)
+      }
+    })
+  },
+  stopRecord: function () {
+    this.setData({
+      speechText: '按住 说话'
+    })
+    wx.stopRecord();
+  },
+  playSpeech: function (event) {
+    var that = this;
+    var filePath = event.currentTarget.dataset.filepath;
+    that.setData({
+      playingSpeech: filePath
+    });
+    var num = 1;
+    var interval = setInterval(function () {
+      that.setData({
+        speechIcon: '/images/speech' + num % 3 + '.png'
+      });
+      num++;
+    }, 500);
+    wx.playVoice({
+      filePath: filePath,
+      complete: function () {
+        clearInterval(interval);
+        that.setData({
+          speechIcon: '/images/speech0.png',
+          playingSpeech: ''
+        });
+      }
+    })
+  },
+  playRobotSpeech: function (event) {
+    var that = this;
+    var filePath = event.currentTarget.dataset.filepath;
+    that.setData({
+      playingSpeech: filePath
+    });
+    var num = 1;
+    var interval = setInterval(function () {
+      that.setData({
+        speechIcon: '/images/speech' + num % 3 + '.png'
+      });
+      num++;
+    }, 500);
+    wx.playBackgroundAudio({
+      dataUrl: filePath
+    });
+    wx.onBackgroundAudioStop(function () {
+      clearInterval(interval);
+      that.setData({
+        speechIcon: '/images/speech0.png',
+        playingSpeech: ''
+      });
+    })
+  }
+})
