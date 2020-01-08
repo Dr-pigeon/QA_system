df84af7a6fe58a4cc60176e397edfbefe1c8a300
MagicianChen <q1218605102@gmail.com>
2020-01-08 12:10:51 +0800

    updated

--- test_xml/testing_xml.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-import xmltodict
-
-k = "\u6fb3\u9580\u5927\u5b78\u5728\u54ea\ud855\ude83"
-k = k.encode('utf-8').decode('unicode_escape')
-
-with open('junk', 'w', encoding='utf-8') as f:
-	f.write(k)
--- test_xml/testing_xml2.py
+++
@@ -1,2 +0,0 @@
-str = '\\u6fb3\\u95e8\\u5927\\u5b66\\u6821\\u957f\\u662f\\u8c01'
-print (str.encode('utf-8').decode('unicode_escape'))
